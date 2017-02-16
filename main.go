0c1ec1cd6c1f40d48065caca1dc2579d689d20ff
Jamie Alquiza <jamiealquiza@gmail.com>
2017-02-15 17:11:31 -0700

    updated comments

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 // Package collider provides a lock-free
-// ring buffer for arbitrary Go objects.
+// circular data structure for arbitrary Go objects.
 package collider
 
 import (
 	"sync/atomic"
 )
 
-// Ring implements a false-sharing
-// resistant ring buffer with slots
-// that hold arbitrary Go objects.
+// Ring is a write-once read-many circular data
+// structure with slots that hold arbitrary Go objects.
 type Ring struct {
 	p0 [64]byte
 	pos uint64
@@ -36,7 +35,8 @@ func (r *Ring) Get() interface{} {
 	return r.slots[(atomic.AddUint64(&r.pos, 1)-1)&(*r.mask)]
 }
 
-// Add adds an item to the next open slot.
+// Add adds an item at the current slot and
+// atomically increments the index.
 func (r *Ring) Add(u interface {}) {
 	r.slots[(atomic.AddUint64(&r.pos, 1)-1)&(*r.mask)] = u
 }
